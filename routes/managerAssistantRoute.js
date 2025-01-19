c89fe6a97633aa3d043e6c6d7ae2dcae51bd81f9
Abedalla Sbeata <abedallasbeata@gmail.com>
2025-01-19 13:24:06 +0200

    simple

--- routes/managerAssistantRoute.js
+++ routes/managerAssistantRoute.js
@@ -2,16 +2,14 @@ const express = require("express");
 const router = express.Router();
 const authService = require("../services/authService");
 
-
-const { generateSchedules } = require("../services/managerAssistantService");
-const { generateSchedulesValidator } = require("../utils/validators/managerAssistantValidator");
-
+const { createSchedule } = require("../services/managerAssistantService");
+const { createScheduleValidator } = require("../utils/validators/managerAssistantValidator");
 
 router.use(authService.protect);
 router.use(authService.allowedTo("manager assistant"));
 
 router
-  .route("/generateSchedules")
-  .post(generateSchedulesValidator, generateSchedules);
+  .route("/createSchedule")
+  .post(createScheduleValidator, createSchedule);
 
 module.exports = router;
--- utils/validators/managerAssistantValidator.js
+++ utils/validators/managerAssistantValidator.js
@@ -1,21 +1,32 @@
 const { check } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
 
-exports.generateSchedulesValidator = [
-  check("class_ids")
+exports.createScheduleValidator = [
+  check("class_id")
+    .notEmpty()
+    .withMessage("معرف الفصل مطلوب"),
+  check("schedule")
     .isArray({ min: 1 })
-    .withMessage("يجب إضافة معرفات الفصول"),
-  check("teacher_ids")
+    .withMessage("يجب إضافة جدول يحتوي على أيام وفترات"),
+  check("schedule.*.day")
+    .notEmpty()
+    .withMessage("اليوم مطلوب")
+    .isIn(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"])
+    .withMessage("اليوم غير صحيح"),
+  check("schedule.*.periods")
     .isArray({ min: 1 })
-    .withMessage("يجب إضافة أرقام هويات المعلمين"),
-  check("subject_ids")
-    .isArray({ min: 1 })
-    .withMessage("يجب إضافة معرفات المواد"),
-  check("available_times.days")
-    .isArray({ min: 1 })
-    .withMessage("يجب إضافة الأيام المتاحة"),
-  check("available_times.times")
-    .isArray({ min: 1 })
-    .withMessage("يجب إضافة الأوقات المتاحة"),
+    .withMessage("يجب إضافة فترات لهذا اليوم"),
+  check("schedule.*.periods.*.subject_id")
+    .notEmpty()
+    .withMessage("معرف المادة مطلوب"),
+  check("schedule.*.periods.*.teacher_id")
+    .notEmpty()
+    .withMessage("معرف المعلم مطلوب"),
+  check("schedule.*.periods.*.start_time")
+    .notEmpty()
+    .withMessage("وقت البداية مطلوب"),
+  check("schedule.*.periods.*.end_time")
+    .notEmpty()
+    .withMessage("وقت النهاية مطلوب"),
   validatorMiddleware,
 ];
