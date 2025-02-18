2567f7d8e7b0eaf49ad293f59fd3db1fc89fa18a
Abedalla Sbeata <abedallasbeata@gmail.com>
2025-02-18 16:11:38 +0200

    s

--- models/classModel.js
+++ models/classModel.js
@@ -1,31 +1,22 @@
 const mongoose = require("mongoose");
 
-const classSchema = new mongoose.Schema(
-  {
-    class_number: Number,
-    level_number: {
-      type: mongoose.Schema.ObjectId, // تغيير النوع إلى ObjectId
-      ref: "Level", // الإشارة إلى مودل Level
-    },
-    numberOfStudents: {
-      type: Number,
-      default: 0,
-    },
-    numberOfTeachers: {
-      type: Number,
-      default: 0,
-    },
-    teachersIDs: [{
-      type: mongoose.Schema.ObjectId, // استخدام ObjectId للمعرفات
-      ref: "User", // الإشارة إلى مودل المستخدمين (إذا كنت تستخدم مودل User)
-    }],
-    studentsIDs: [{
-      type: mongoose.Schema.ObjectId, // استخدام ObjectId بدلاً من String
-      ref: "User",
-    }],
+const classSchema = new mongoose.Schema({
+  class_number: Number,
+  level_number: {
+    type: Number,
+    ref: "Level",
   },
-  { timestamps: true }
-);
+  numberOfStudents: {
+    type: Number,
+    default: 0,
+  },
+  numberOfTeachers: {
+    type: Number,
+    default: 0,
+  },
+  teachersIDs: [String],
+  studentsIDs: [String],
+}, {timestamps: true});
 
-const Class = mongoose.model("Class", classSchema);
-module.exports = Class;
+const classModel = mongoose.model("Class", classSchema);
+module.exports = classModel;
