ce12a4ca292c917b959cac27917c57610be6c9d6
Abedalla Sbeata <abedallasbeata@gmail.com>
2025-02-18 16:09:34 +0200

    S

--- models/classModel.js
+++ models/classModel.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 
-const classSchema = new mongoose.Schema({
-  class_number: Number,
-  level_number: {
-    type: Number,
-    ref: "Level",
+const classSchema = new mongoose.Schema(
+  {
+    class_number: Number,
+    level_number: {
+      type: mongoose.Schema.ObjectId, // تغيير النوع إلى ObjectId
+      ref: "Level", // الإشارة إلى مودل Level
+    },
+    numberOfStudents: {
+      type: Number,
+      default: 0,
+    },
+    numberOfTeachers: {
+      type: Number,
+      default: 0,
+    },
+    teachersIDs: [{
+      type: mongoose.Schema.ObjectId, // استخدام ObjectId للمعرفات
+      ref: "User", // الإشارة إلى مودل المستخدمين (إذا كنت تستخدم مودل User)
+    }],
+    studentsIDs: [{
+      type: mongoose.Schema.ObjectId, // استخدام ObjectId بدلاً من String
+      ref: "User",
+    }],
   },
-  numberOfStudents: {
-    type: Number,
-    default: 0,
-  },
-  numberOfTeachers: {
-    type: Number,
-    default: 0,
-  },
-  teachersIDs: [String],
-  studentsIDs: [String],
-}, {timestamps: true});
+  { timestamps: true }
+);
 
-const classModel = mongoose.model("Class", classSchema);
-module.exports = classModel;
+const Class = mongoose.model("Class", classSchema);
+module.exports = Class;
--- models/levelModel.js
+++ models/levelModel.js
@@ -2,47 +2,65 @@ const mongoose = require("mongoose");
 const Subject = require("./subjectModel");
 const Class = require("./classModel");
 
-const levelSchema = new mongoose.Schema({
-  level_number: {
-    type: Number,
-    unique: true,
+const levelSchema = new mongoose.Schema(
+  {
+    level_number: {
+      type: Number,
+      unique: true,
+    },
+    level_name: String,
+    classes: [{
+      type: mongoose.Schema.ObjectId,
+      ref: "Class",
+    }],
+    available_subjects: [String],
+    numberOfStudents: {
+      type: Number,
+      default: 0,
+    },
+    numberOfTeachers: {
+      type: Number,
+      default: 0,
+    },
+    numberOfClasses: {
+      type: Number,
+      default: 0,
+    },
   },
-  level_name: String,
-  classes: [Number],
-  available_subjects: [String],
-  numberOfStudents: {
-    type: Number,
-    default: 0,
-  },
-  numberOfTeachers: {
-    type: Number,
-    default: 0,
-  },
-  numberOfClasses: {
-    type: Number,
-    default: 0,
-  },
-}, {timestamps:true});
+  { timestamps: true }
+);
 
 levelSchema.pre("save", async function (next) {
-  const documents = await Subject.find().countDocuments();
-  for (let i = 0; i < documents; i++) {
-    if ((await Subject.find()).at(i).levels.includes(this.level_number)) {
-      this.available_subjects.push((await Subject.find()).at(i).subject_name);
-    }
+  try {
+    // جلب المواد التي تحتوي على هذا المستوى
+    const subjects = await Subject.find({ levels: this.level_number });
+
+    // استخراج أسماء المواد وإضافتها
+    this.available_subjects = subjects.map((subject) => subject.subject_name);
+
+    // البحث عن الكلاسات وتحويل الأرقام إلى ObjectId
+    const classIds = await Class.find({ class_number: { $in: this.classes } }).select("_id");
+
+    this.classes = classIds.map((cls) => cls._id);
+
+    // تحديث عدد الكلاسات
+    this.numberOfClasses = this.classes.length;
+
+    next();
+  } catch (error) {
+    next(error);
   }
-  this.numberOfClasses = this.classes.length;
-  next();
 });
 
 levelSchema.post("save", async function () {
-  for (let i = 0; i < this.classes.length; i++) {
-    await Class.create({
-      class_number: this.classes[i],
-      level_number: this.level_number,
-    });
+  try {
+    for (const classId of this.classes) {
+      await Class.findByIdAndUpdate(classId, { level_number: this.level_number });
+    }
+  } catch (error) {
+    console.error("Error updating classes with level_number:", error);
   }
 });
 
-const levelModel = mongoose.model("Level", levelSchema);
-module.exports = levelModel;
+const Level = mongoose.model("Level", levelSchema);
+module.exports = Level;
