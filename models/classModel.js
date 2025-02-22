7289d876ff445273cca20eea000d5fb71ab3aec5
Abedalla Sbeata <abedallasbeata@gmail.com>
2025-02-22 14:13:58 +0200

    s

--- models/classModel.js
+++ models/classModel.js
@@ -1,25 +1,41 @@
 const mongoose = require("mongoose");
-const Subject = require('../models/subjectModel')
+const Subject = require("../models/subjectModel");
+const ClassSubject = require("../models/classSubject");
 
-const classSchema = new mongoose.Schema({
-  class_number: Number,
-  level_number: {
-    type: Number,
-    ref: "Level",
+const classSchema = new mongoose.Schema(
+  {
+    class_number: Number,
+    level_number: {
+      type: Number,
+      ref: "Level",
+    },
+    numberOfStudents: {
+      type: Number,
+      default: 0,
+    },
+    numberOfTeachers: {
+      type: Number,
+      default: 0,
+    },
+    teachersIDs: [String],
+    studentsIDs: [String],
   },
-  numberOfStudents: {
-    type: Number,
-    default: 0,
-  },
-  numberOfTeachers: {
-    type: Number,
-    default: 0,
-  },
-  teachersIDs: [String],
-  studentsIDs: [String],
-}, {timestamps: true});
+  { timestamps: true }
+);
 
+// بعد الحفظ: إنشاء ClassSubject لكل مادة مرتبطة بالمستوى
+classSchema.post("save", async function () {
+  // جلب المواد الخاصة بهذا المستوى
+  const subjects = await Subject.find({ levels: this.level_number });
 
+  // إنشاء ClassSubject لكل مادة
+  for (const subject of subjects) {
+    await ClassSubject.create({
+      class_id: this._id,
+      subject_id: subject._id,
+    });
+  }
+});
 
 const classModel = mongoose.model("Class", classSchema);
 module.exports = classModel;
