dff7667a9ae25ed7c90a1f3c40d87ef831e5da5f
Abedalla Sbeata <abedallasbeata@gmail.com>
2025-02-22 13:57:37 +0200

    s

--- models/levelModel.js
+++ models/levelModel.js
@@ -1,46 +1,63 @@
 const mongoose = require("mongoose");
 const Subject = require("./subjectModel");
 const Class = require("./classModel");
+const ClassSubject = require("./subjectModel");
 
-const levelSchema = new mongoose.Schema({
-  level_number: {
-    type: Number,
-    unique: true,
+const levelSchema = new mongoose.Schema(
+  {
+    level_number: {
+      type: Number,
+      unique: true,
+    },
+    level_name: String,
+    classes: [Number],
+    available_subjects: [String],
+    numberOfStudents: {
+      type: Number,
+      default: 0,
+    },
+    numberOfTeachers: {
+      type: Number,
+      default: 0,
+    },
+    numberOfClasses: {
+      type: Number,
+      default: 0,
+    },
   },
-  level_name: String,
-  classes: [Number],
-  available_subjects: [String],
-  numberOfStudents: {
-    type: Number,
-    default: 0,
-  },
-  numberOfTeachers: {
-    type: Number,
-    default: 0,
-  },
-  numberOfClasses: {
-    type: Number,
-    default: 0,
-  },
-}, {timestamps:true});
+  { timestamps: true }
+);
 
+// قبل الحفظ: تحديد المواد المتاحة لهذا المستوى
 levelSchema.pre("save", async function (next) {
-  const documents = await Subject.find().countDocuments();
-  for (let i = 0; i < documents; i++) {
-    if ((await Subject.find()).at(i).levels.includes(this.level_number)) {
-      this.available_subjects.push((await Subject.find()).at(i).subject_name);
-    }
-  }
+  const subjects = await Subject.find();
+  this.available_subjects = subjects
+    .filter((subject) => subject.levels.includes(this.level_number))
+    .map((subject) => subject.subject_name);
+
   this.numberOfClasses = this.classes.length;
   next();
 });
 
+// بعد الحفظ: إنشاء الفصول وربطها بالمواد المتاحة
 levelSchema.post("save", async function () {
   for (let i = 0; i < this.classes.length; i++) {
-    await Class.create({
+    // إنشاء الكلاس
+    const newClass = await Class.create({
       class_number: this.classes[i],
       level_number: this.level_number,
     });
+
+    // جلب المواد المتاحة لهذا المستوى
+    const subjects = await Subject.find({ subject_name: { $in: this.available_subjects } });
+
+    // إنشاء ClassSubject لكل مادة متاحة وربطها بالكلاس الجديد
+    for (const subject of subjects) {
+      await ClassSubject.create({
+        class_id: newClass._id,
+        subject_id: subject._id,
+      });
+    }
   }
 });
 
