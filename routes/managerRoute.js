5194ae7bfac6295b96ebda746a37dbed2ef13dbf
AbedallaSbeata <abedallasbeata@email.com>
2025-03-11 14:38:14 +0200

    s

--- routes/managerRoute.js
+++ routes/managerRoute.js
@@ -152,6 +152,6 @@ router.route('/getSubmissionsByActivity/:activity_id').get(getSubmissionsByActiv
 router.route('/updateSubmission/:submission_id').put(updateSubmission);
 router.route('/deleteSubmissions').delete(deleteSubmissions);
 router.route('/getClassGrades/:class_id').get(getClassGrades);
-router.route('/getStudentGrades/:student_id').post(getStudentGrades);
+router.route('/getStudentGrades/:student_id').get(getStudentGrades);
 
 module.exports = router;
